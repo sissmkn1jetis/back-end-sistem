0dcbbe737e44b8ebf97369587b7e7d3935cd56c6
EC2 Default User <ec2-user@ip-172-31-9-156.ap-southeast-1.compute.internal>
2023-05-28 01:54:03 +0000

    1.Unggah Pertama

---
+++ server.js
@@ -0,0 +1,14 @@
+const routes = require('./routes');
+const Hapi = require('@hapi/hapi');
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: '0.0.0.0',
+    });
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
