9ba0982c28def54f5ac9f14d3ea3505c3aebd4b5
Denmas12 <m.jawahir48@gmail.com>
2023-06-02 21:14:34 +0700

    hapus zip

--- routes.js
+++ routes.js
@@ -13,6 +13,38 @@ const routes = [
             return 'Homepage V3';
         },
     },
+    {
+        method: 'GET',
+        path: '/hello/{name}',
+        handler: function (request, h) {
+    
+            const name = request.params.name;
+            return 'Hello ' + name
+        }
+    },
+    {
+        method: 'GET',
+        path: '/home',
+        handler: function (request, h) {
+    
+            return h.redirect('/');
+        }
+    },
+    {
+        method: 'GET',
+        path: '/user',
+        handler: function (request, h) {
+    
+            const user = {
+                firstName: 'John',
+                lastName: 'Doe',
+                userName: 'JohnDoe',
+                id: 123
+            }
+    
+            return user;
+        }
+    }
 ];
 
 module.exports = routes;
