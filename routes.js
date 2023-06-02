de2a41a7acac4d86385c557eb13e928697552b33
Denmas12 <m.jawahir48@gmail.com>
2023-06-02 17:01:05 +0700

    update ke - 2

--- routes.js
+++ routes.js
@@ -3,14 +3,14 @@ const routes = [
         method: '*',
         path: '/',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
+            return 'Halaman tidak dapat diakses dengan method tersebut V3';
         },
     },
     {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return 'Homepage V2';
+            return 'Homepage V3';
         },
     },
 ];
